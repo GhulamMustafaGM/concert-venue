792b6c9bef552fa44436c0f6a1e90ea6347fd051
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-15 18:32:17 +0200

    updated concert venue app

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -4,7 +4,7 @@ import Slide from 'react-reveal/Slide';
 class TimeUntil extends Component {
 
     state = {
-        deadline: 'Dec, 20, 2020',
+        deadline: 'Dec, 20, 2023',
         days:'0',
         hours:'0',
         minutes:'0',
