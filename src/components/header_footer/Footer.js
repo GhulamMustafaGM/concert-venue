d6a7b668e4a5c99bd52d917bbb12b6806d5cb616
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-15 18:28:58 +0200

    updated app

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Fade delay={500}>
                 <div className="font_righteous footer_logo_venue">ConcertVenue</div>
                 <div className="footer_copyright">
-                    ConcertVenue 2020.All rights reserved.
+                    ConcertVenue 2025.All rights reserved.
                 </div>
             </Fade>
         </footer>
